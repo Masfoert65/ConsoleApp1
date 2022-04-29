6f2bfaf9872f381fc89e51c5b335bd03ba5f71f5
masfo <masfo@DESKTOP-EB7US4N>
2022-04-29 14:35:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Interface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Interface
+
+    {
+
+    
+        interface Iaccount
+        {
+            // Положить деньги на счет
+            void put(double summa);
+            // Снять деньги со счета 
+            void get(double summa);
+            // Начислить проценты
+            void percent();
+        }
+        interface Ibonus
+        {
+            // начислить бонус
+            double bonus();
+        }
+    }
+}
+
+
